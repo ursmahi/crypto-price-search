9df588eca9a4fbcc71bda6f5b6c3919aefe8c278
Mahi <170040338@kluniversity.in>
2022-03-27 12:07:59 +0530

    Crypto Price Searc App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react';
 
+import Axios from 'axios';
+import Coin from './Components/Coin';
 function App() {
+  const [searchWord,setSearchWord] = useState('');
+
+  const [listOfCoins,setListOfCoins]=useState([])
+  useEffect(() => {
+    Axios.get('https://api.coinstats.app/public/v1/coins?skip=0&currency=INR').then(
+      (response) => {
+        setListOfCoins(response.data.coins)
+      }
+    )
+  },[]);
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return (coin.name.toLowerCase().includes(searchWord.toLowerCase())
+    || coin.symbol.toLowerCase().includes(searchWord.toLowerCase())
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cryptoHeader">
+      <input type="text" placeholder="Search for a coin" onChange={
+        (event)=>{
+          setSearchWord(event.target.value)
+        }
+        }/>
+      </div>
+
+      <div className="cryptoDisplay">
+        {filteredCoins.map(
+          (coin)=>{
+          return( <Coin name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol}/> )
+          }
+        )}
+        </div>
     </div>
   );
 }
---
+++ src/Components/Coin.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './../App.css'
+function Coin({name,icon,price,symbol}) { 
+  const priceIndian =(price)=>{
+    var x=price;
+    x=x.toString();
+    var afterPoint = '';
+    if(x.indexOf('.') > 0)
+       afterPoint = x.substring(x.indexOf('.'),x.length);
+    x = Math.floor(x);
+    x=x.toString();
+    var lastThree = x.substring(x.length-3);
+    var otherNumbers = x.substring(0,x.length-3);
+    if(otherNumbers != '')
+        lastThree = ',' + lastThree;
+    var res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree + afterPoint;
+    
+    return res
+  }
+  return (
+    <div className="coin">
+        <div>
+          <h1>{name}</h1>
+          <h3>{symbol}</h3>
+        </div>
+            <img src={icon} alt=""/>
+        <h3>Price: {priceIndian(price.toFixed(3))} INR</h3>
+    </div>
+  )
+}
+
+export default Coin
